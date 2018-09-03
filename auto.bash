5d66b84fcec3298aea13145e7ab3c59ae5806425
des338 <des338@cs.drexel.edu>
2018-09-03 10:06:18 -0400

    Removed old files and made app

--- auto.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf mutFund.csv
-
-echo "Ticker,Category,Star,Net,YTD,1yr,3yr,5yr,10yr" >> mutFund.csv
-for name in $(cat input.txt) ; do
-	python3 fund.py $name >> mutFund.csv
-done
--- auto.py
+++ auto.py
@@ -1,50 +1,7 @@
+import os
 import sys
 import json
 import requests
-
-def readJSONFile(filename):
-	with open(filename, 'r') as f:
-		jsonObj = json.load(f)
-	return getCusip(jsonObj)
-
-def getCusip(jsonObj, ticker):
-	for elm in jsonObj:
-		return jsonObj[ticker]['Cusip']
-
-def readTickers(filename):
-	with open(filename) as f:
-		content = f.readline()
-	content = [x.strip() for x in content]
-
-def tickerToCusip(content):
-	ll = []
-	for ticker in content:
-		ll.append(ticker + ":" + getCusip(readJSONFile((ticker+".json)"))))
-
-def createFiles(filename):
-	content = []
-	with open(filename, 'r') as ins:
-		for line in ins:
-			line=line.strip()
-			jsonFile = line + ".json"
-			params = (
-			    ('fIds', line),
-			)
-			response = requests.get('https://www.fidelity.com/evaluator/compare', params=params)
-
-			res = []
-
-			if line in response.json().keys():
-				# print(line + "," + getCusip(response.json(), line))
-				res.append(line + "," + getCusip(response.json(), line))
-			else:
-				# print(line)
-				res.append(line)
-
-			with open('tickersCusips.txt', 'w') as file_handler:
-			    for item in res:
-			    	print(item)
-			    	file_handler.write("{}\n".format(item))
 
 def getCusip2(jsonObj):
 	for elm in jsonObj:
@@ -60,7 +17,6 @@ def getData(content):
 	data['3yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns3yr']
 	data['5yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns5yr']
 	data['10yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns10yr']
-	# print(data)
 	print('{} {} {} {} {} {} {} {}'.format(data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
 
 def createFiles2(cusip):
@@ -68,14 +24,11 @@ def createFiles2(cusip):
 	response = requests.get(htmlFile)
 	getCusip2(response.json())
 
-
-
-
-def getCusip3(jsonObj, ticker):
+def getCusip(jsonObj, ticker):
 	for elm in jsonObj:
-		return(getData2(jsonObj['model'], ticker))
+		return(getData(jsonObj['model'], ticker))
 
-def getData2(content, ticker):
+def getData(content, ticker):
 	data = {}
 	try:
 		data['net'] = content['DetailsData']['detailDataList'][0]['expRatioGross']['value']
@@ -85,57 +38,44 @@ def getData2(content, ticker):
 		data['star'] = content['MStarRatingsData']['stardata'][0]['starOverallRating']
 	except:
 		data['star'] = "N/A"
-
 	try:
 		data['category'] = content['FundInformationData']['fundInformationData']['mstarCtgyName']
 	except:
 		data['category'] = "N/A"
-
 	try:
 		data['ytd'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['cumulativeReturnsYtd']
 	except:
 		data['ytd'] = "N/A"
-
 	try:
 		data['net'] = content['DetailsData']['detailDataList'][0]['expRatioGross']['value']
 	except:
 		data['net'] = "N/A"
-
 	try:
 		data['1yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns1yr']
 	except:
 		data['1yr'] = "N/A"
-	
 	try:
 		data['3yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns3yr']
 	except:
 		data['3yr'] = "N/A"
-
 	try:
 		data['5yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns5yr']
 	except:
 		data['5yr'] = "N/A"
-	
 	try:
 		data['10yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns10yr']
 	except:
 		data['10yr'] = "N/A"
-	# print(data)
-	# print('{} {} {} {} {} {} {} {} {}'.format(ticker, data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
-	
-
-	# print('{},{},{},{},{},{},{},{},{}'.format(ticker, data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
 	return('{},{},{},{},{},{},{},{},{}'.format(ticker, data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
-	
 
-def createFiles3(cont):
+def createFiles(cont):
 	words = cont.split(',')
 	ticker = words[0]
 	cusip = words[1]
 	htmlFile = 'https://fundresearch.fidelity.com/api/mutual-funds/header/' + cusip
 	response = requests.get(htmlFile)
 	try:
-		return(getCusip3(response.json(), ticker))
+		return(getCusip(response.json(), ticker))
 	except:
 		return(('{} - !!!! NO DATA FOUND'.format(ticker)))
 
@@ -144,21 +84,28 @@ def listMaker(filename):
 		lines = f.read().splitlines()
 	return lines
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
 
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
-
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush() 
 
 if __name__ == "__main__":
-	# createFiles(sys.argv[1])
-	# createFiles2(sys.argv[1])
-
+	if os.path.isfile("output.csv"):
+		os.remove("output.csv")
 	res = []
 	res.append("Ticker,Category,Stars,NET,YTD,1yr,3yr,5yr,10yr")
 
 	lines = listMaker('input.csv')
+	cnt = 0
 	for elem in lines:
-		res.append(createFiles3(elem))
+		progress(cnt, (len(lines)-1))
+		cnt+=1
+		res.append(createFiles(elem))
 
-	# print(res)
 	with open('output.csv', 'w') as file_handle:
 		file_handle.writelines("%s\n" % place for place in res)
