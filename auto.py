1b534fd50310ed5d4ce233c34e13e81b4c90d064
des338 <des338@cs.drexel.edu>
2018-09-28 14:44:15 -0400

    Added comments, view cusip, and removed not pick

--- auto.py
+++ auto.py
@@ -1,35 +1,22 @@
 import os
 import sys
+import csv
+import operator
 import json
 import requests
 
-def getCusip2(jsonObj):
+def getCusip(jsonObj, ticker, cusip, extra):
 	for elm in jsonObj:
-		getData(jsonObj['model'])
+		return(getData(jsonObj['model'], ticker, cusip, extra))
 
-def getData(content):
+def getData(content, ticker, cusip, extra):
 	data = {}
-	data['net'] = content['DetailsData']['detailDataList'][0]['expRatioGross']['value']
-	data['star'] = content['MStarRatingsData']['stardata'][0]['starOverallRating']
-	data['category'] = content['FundInformationData']['fundInformationData']['mstarCtgyName']
-	data['ytd'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['cumulativeReturnsYtd']
-	data['1yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns1yr']
-	data['3yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns3yr']
-	data['5yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns5yr']
-	data['10yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns10yr']
-	print('{} {} {} {} {} {} {} {}'.format(data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
 
-def createFiles2(cusip):
-	htmlFile = 'https://fundresearch.fidelity.com/api/mutual-funds/header/' + cusip
-	response = requests.get(htmlFile)
-	getCusip2(response.json())
-
-def getCusip(jsonObj, ticker):
-	for elm in jsonObj:
-		return(getData(jsonObj['model'], ticker))
-
-def getData(content, ticker):
-	data = {}
+	try:
+		if len(content['FundInformationData']['fundInformationData']['publicationNm']) > 0:
+			data['pick'] = "Pick"
+	except:
+		data['pick'] = ""
 	try:
 		data['net'] = content['DetailsData']['detailDataList'][0]['expRatioGross']['value']
 	except:
@@ -66,16 +53,20 @@ def getData(content, ticker):
 		data['10yr'] = content['PerformanceAvgAnnualReturnsData']['performanceAvgAnnualReturnsDataList'][0]['fundPerformanceRowDataList'][0]['averageAnnualReturns10yr']
 	except:
 		data['10yr'] = "N/A"
-	return('{},{},{},{},{},{},{},{},{}'.format(ticker, data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr']))
+	return('{},{},{},{},{},{},{},{},{},{},{},{}'.format(ticker, data['pick'], data['category'], data['star'], data['net'], data['ytd'], data['1yr'], data['3yr'], data['5yr'], data['10yr'], cusip, extra))
 
 def createFiles(cont):
 	words = cont.split(',')
 	ticker = words[0]
 	cusip = words[1]
+	try:
+		extra = words[2]
+	except:
+		extra = ""
 	htmlFile = 'https://fundresearch.fidelity.com/api/mutual-funds/header/' + cusip
 	response = requests.get(htmlFile)
 	try:
-		return(getCusip(response.json(), ticker))
+		return(getCusip(response.json(), ticker, cusip, extra))
 	except:
 		return(('{} - !!!! NO DATA FOUND'.format(ticker)))
 
@@ -94,11 +85,11 @@ def progress(count, total, status=''):
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush() 
 
+
 if __name__ == "__main__":
 	if os.path.isfile("output.csv"):
 		os.remove("output.csv")
 	res = []
-	res.append("Ticker,Category,Stars,NET,YTD,1yr,3yr,5yr,10yr")
 
 	lines = listMaker('input.csv')
 	cnt = 0
@@ -107,5 +98,15 @@ if __name__ == "__main__":
 		cnt+=1
 		res.append(createFiles(elem))
 
+	# print(res)
+
+	# sortList = sorted(res, key=lambda x: x.split(',')[2])
+	sortList = res
+	sortList.insert(0,"Ticker,Pick/Not,Category,Stars,NET,YTD,1yr,3yr,5yr,10yr,Cusip,Code")
+	# sortedList = sortIt(res)
+	# sortedList.insert(0,"Ticker,Pick/Not,Category,Stars,NET,YTD,1yr,3yr,5yr,10yr,Cusip,Code")
+
+
 	with open('output.csv', 'w') as file_handle:
-		file_handle.writelines("%s\n" % place for place in res)
+		file_handle.writelines("%s\n" % place for place in sortList)
+		# file_handle.writelines("%s\n" % place for place in res)
